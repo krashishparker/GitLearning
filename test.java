0c4ecf511184e1c345f8a75786e1aec55e08f9e4
Ashish Kumar (180153) <ashishk8@amdocs.com>
2021-07-07 19:29:15 +0530

    2x_changes

--- test.java
+++ test.java
@@ -3,8 +3,8 @@ public class Calculate
 public static void main(String args[])   
 {  
 int n1=225, n2=115, sum;  
-sum = n1+n2;  
-int sub= n1-n2;
+sum = 2*n1+2*n2;  
+int sub= 2*n1-2*n2;
 System.out.println("The sum of numbers is: "+sum);  
 System.out.println("The difference of numbers is: "+sub); 
 }  
