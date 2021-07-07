3d4e71cf69781220b4a56802b212416a26533af0
Ashish Kumar (180153) <ashishk8@amdocs.com>
2021-07-07 19:40:11 +0530

    4x_change

--- test.java
+++ test.java
@@ -1,10 +1,10 @@
-public class Calculate  
+public class Calculate 
 {  
 public static void main(String args[])   
 {  
 int n1=225, n2=115, sum;  
-sum = 2*n1+2*n2;  
-int sub= 2*n1-2*n2;
+sum = 4*n1+4*n2;  
+int sub= 4*n1-4*n2;
 System.out.println("The sum of numbers is: "+sum);  
 System.out.println("The difference of numbers is: "+sub); 
 }  
