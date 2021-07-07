1d6c930365a6dee9d03d2f79a28860c99036bfef
Ashish Kumar (180153) <ashishk8@amdocs.com>
2021-07-07 19:09:51 +0530

    First commit

---
+++ test.java
@@ -0,0 +1,12 @@
+public class Calculate  
+{  
+public static void main(String args[])   
+{  
+int n1=225, n2=115, sum;  
+sum = n1+n2;  
+int sub= n1-n2;
+System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The difference of numbers is: "+sub); 
+}  
+
+}  
